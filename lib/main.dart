af559782ae8608c3c9269c23ab50830bd99c741e
ccheeems <156826924+ccheeems@users.noreply.github.com>
2024-03-13 10:50:15 +0800

    *Cheems-Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,46 @@ class DictionaryApp extends StatelessWidget {
   }
 }
 
+class DictionaryHomePage extends StatefulWidget {
+  @override
+  _DictionaryHomePageState createState() => _DictionaryHomePageState();
+}
+
+class _DictionaryHomePageState extends State<DictionaryHomePage> {
+  String _currentWord = '';
+  String _definition = '';
+  TextEditingController _controller = TextEditingController();
+
+  Future<void> _searchWord(String word) async {
+    final response = await http.get(
+        Uri.parse('https://api.dictionaryapi.dev/api/v2/entries/en/$word'));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      if (data.isNotEmpty) {
+        final String mainWord = data[0]['word'];
+        final String phonetics =
+            data[0]['phonetics'][0]['text'] ?? 'No phonetics available';
+        final meanings = data[0]['meanings'];
+        if (meanings.isNotEmpty) {
+          final definitions = meanings[0]['definitions'];
+          if (definitions.isNotEmpty) {
+            final definition = definitions[0]['definition'];
+            setState(() {
+              _definition =
+                  '$mainWord [$phonetics]:\n\n DEFINITION: \n $definition';
+            });
+            return;
+          }
+        }
+      }
+    } else {
+      setState(() {
+        _definition = 'Definition not found.';
+      });
+    }
+  }
+  
+
 
 
               
